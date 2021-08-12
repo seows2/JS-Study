32066411962b11735c4d5238ea07a1caaa884e52
seows2 <seows2@naver.com>
2021-08-12 09:48:45 +0900

    잘못된 표현 수정, 폴더 구분

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-function Func() {}
-Func.prototype.num = 2;
-const a = new Func();
-a.num = 1;
-console.log(Func.prototype.num); // { num: 2 }
-console.log(a); // Func { num: 1 }
-console.log(a.num); // 1
+let value = true;
+console.log(typeof value);
