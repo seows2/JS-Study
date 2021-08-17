ac3bff6aa86b62faba72fe53b83c39cd31cadb6b
seows2 <seows2@naver.com>
2021-08-17 17:55:43 +0900

    수정 중

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-const today = new Date();
-const dateString = today.toLocaleDateString("en-US", {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-});
-const dayName = today.toLocaleDateString("en-US", { weekday: "long" });
-
-console.log(dateString, dayName);
+const arr = ["zero", "one", "two"];
+const obj1 = {
+  0: "zero",
+  1: "one",
+  2: "two",
+  length: 3,
+};
+console.log(arr[1]); // 'one'
+console.log(arr.length); // 3
+console.log(typeof arr); // object
+console.log(obj1[1]); // 'one'
+console.log(obj1.length); // 3
+console.log(typeof obj1); // object
