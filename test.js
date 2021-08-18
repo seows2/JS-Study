90c5d25456f9addd09883fa11f98d8aebda3c58a
seows2 <seows2@naver.com>
2021-08-18 15:55:39 +0900

    asd

--- test.js
+++ test.js
@@ -1,4 +1,2 @@
-const points = [40, 100, 1, 5, 2, 25, 10];
-const sort1 = points.sort(); // [1, 10, 100, 2, 25, 40, 5]
-const sort2 = points.sort((a, b) => a - b); // [1, 2, 5, 10, 25, 40, 100]
-const sort3 = points.sort((a, b) => b - a); // [ 100, 40, 25, 10, 5, 2, 1 ]
+const arr2 = Array.from({ length: 3 });
+console.log(arr2);
