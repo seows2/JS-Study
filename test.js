9206beecd180c087b20556cc4f01cfd02386b2ef
seows2 <seows2@naver.com>
2021-08-18 14:28:41 +0900

    마무리

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-const result = [1, 2, 3, 4, 5].filter((elm) => elm % 2);
+const users = [
+  { id: 1, name: "Lee" },
+  { id: 2, name: "Kim" },
+  { id: 2, name: "Choi" },
+  { id: 3, name: "Park" },
+];
 
-console.log(result); // [ 1, 3, 5 ]
+// 콜백함수를 실행하여 그 결과가 참인 첫번째 요소를 반환한다.
+let result = users.findIndex((item) => item.id === 2); // { id: 2, name: 'Kim' }
+
+console.log(result);
