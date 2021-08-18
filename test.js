afc06deca948c02d9e02535c303b8b5a2a126c07
seows2 <seows2@naver.com>
2021-08-18 14:19:03 +0900

    30

--- test.js
+++ test.js
@@ -1,17 +1,3 @@
-const arr1 = Array.from({ length: 3 }, (_, i) => i);
-const arr2 = Array.from({ length: 3 }, () => Array(3).fill(0));
-const arr3 = Array.from(arr1, (elm) => elm * 2);
+const result = [1, 2, 3, 4, 5].filter((elm) => elm % 2);
 
-console.log(arr1); // [ 0, 1, 2 ]
-console.log(arr2); // [ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ]
-console.log(arr3); // [ 0, 2, 4 ]
-console.log(arr1);
-const obj1 = {
-  0: "zero",
-  1: "one",
-  2: "two",
-  length: 3,
-};
-
-const arr4 = Array.from(obj1);
-console.log(arr4);
+console.log(result); // [ 1, 3, 5 ]
