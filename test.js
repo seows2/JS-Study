709ab2bae19632f1f485f9ecb4a57c3693446b4b
seows2 <seows2@naver.com>
2021-08-20 08:50:23 +0900

    프로미스 작성 중

--- test.js
+++ test.js
@@ -1,4 +1,35 @@
-const points = [40, 100, 1, 5, 2, 25, 10];
-const sort1 = points.sort(); // [1, 10, 100, 2, 25, 40, 5]
-const sort2 = points.sort((a, b) => a - b); // [1, 2, 5, 10, 25, 40, 100]
-const sort3 = points.sort((a, b) => b - a); // [ 100, 40, 25, 10, 5, 2, 1 ]
+setTimeout(
+  (name) => {
+    let coffeeList = name;
+    console.log(coffeeList);
+
+    setTimeout(
+      (name) => {
+        coffeeList += ", " + name;
+        console.log(coffeeList);
+
+        setTimeout(
+          (name) => {
+            coffeeList += ", " + name;
+            console.log(coffeeList);
+
+            setTimeout(
+              (name) => {
+                coffeeList += ", " + name;
+                console.log(coffeeList);
+              },
+              500,
+              "Latte"
+            );
+          },
+          500,
+          "Mocha"
+        );
+      },
+      500,
+      "Americano"
+    );
+  },
+  500,
+  "Espresso"
+);
