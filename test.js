036abd7904f36b39fcef170203f3f5cd60ab7cb5
seows2 <seows2@naver.com>
2021-08-23 14:02:04 +0900

    정리 완

--- test.js
+++ test.js
@@ -1,17 +1,5 @@
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, "one");
-});
-
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(reject, 120, "two");
-});
-
-Promise.all([promise1, promise2]).then(
-  (value) => {
-    console.log(value);
-    // Both resolve, but promise2 is faster
-  },
-  (e) => console.log("e", e)
-);
-
-// expected output: "two"
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    throw new Error("에러 발생!");
+  }, 1000);
+}).catch((err) => console.log("catch", err));
